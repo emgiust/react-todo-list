f671c5d56b3b01c020656349e9460c523a35c793
Emily Giust <emilygiust@Emilys-MacBook-Pro.local>
2019-02-04 21:22:13 -0600

    form validation

--- src/ToDo.js
+++ src/ToDo.js
@@ -5,82 +5,90 @@ import './ToDo.css';
 import ToDoItem from './components/ToDoItem';
 
 class ToDo extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         list: [{ todo: 'clean the house' }, { todo: 'pay bills' }],
-         todo: ''
-      };
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [{ todo: 'clean the house' }, { todo: 'pay bills' }],
+      todo: ''
+    };
+  }
 
-   // watches the value entered into the input
-   // updates the state of todo
-   handleInput = e => {
-      this.setState({
-         todo: e.target.value
-      });
-   };
+  // watches the value entered into the input
+  // updates the state of todo
+  handleInput = e => {
+    this.setState({
+      todo: e.target.value
+    });
+  };
 
-   // ensures that a blank item cannot be added
-   // triggers the createNewToDoItem function
-   handleKeyPress = e => {
-      if (e.target.value !== '') {
-         if (e.key === 'Enter') {
-            this.createNewToDoItem();
-         }
+  // ensures that a blank item cannot be added
+  // triggers the createNewToDoItem function
+  handleKeyPress = e => {
+    if (e.target.value !== '') {
+      if (e.key === 'Enter') {
+        this.createNewToDoItem();
       }
-   };
+    }
+  };
 
-   // takes the entire list from state (using a spread operator)
-   // and adds the value entered into the input on the end of the list
-   // todo is set back to an emply string to update the value inside input
-   createNewToDoItem = e => {
-      this.setState(({ list, todo }) => ({
-         list: [...list, { todo }],
-         todo: ''
-      }));
-   };
+  // takes the entire list from state (using a spread operator)
+  // and adds the value entered into the input on the end of the list
+  // todo is set back to an emply string to update the value inside input
+  createNewToDoItem = e => {
+    const { todo } = this.state;
 
-   // this is now being emitted back to the parent from the child component
-   // create a filtered list which does not include the item that triggered
-   // the deleteItem function
-   deleteItem = indexToDelete => {
-      this.setState(({ list }) => ({
-         list: list.filter((toDo, index) => index !== indexToDelete)
-      }));
-   };
+    //check for errors
+    if (todo === '') {
+      console.log('oopsie');
+      return;
+    }
 
-   render(e) {
-      return (
-         <div className="todo-wrapper">
-            <div className="ToDo-Container">
-               <div className="ToDo-Content">
-                  {this.state.list.map((item, key) => {
-                     return (
-                        <ToDoItem
-                           key={key}
-                           item={item.todo}
-                           deleteItem={this.deleteItem.bind(this, key)}
-                        />
-                     );
-                  })}
-               </div>
-            </div>
-            <div className="input-wrapper">
-               <input
-                  type="text"
-                  value={this.state.todo}
-                  placeholder="Your task.."
-                  onChange={this.handleInput}
-                  onKeyPress={this.handleKeyPress}
-               />
-               <button className="add-button" onClick={this.createNewToDoItem}>
-                  +
-               </button>
-            </div>
-         </div>
-      );
-   }
+    this.setState(({ list, todo }) => ({
+      list: [...list, { todo }],
+      todo: ''
+    }));
+  };
+
+  // this is now being emitted back to the parent from the child component
+  // create a filtered list which does not include the item that triggered
+  // the deleteItem function
+  deleteItem = indexToDelete => {
+    this.setState(({ list }) => ({
+      list: list.filter((toDo, index) => index !== indexToDelete)
+    }));
+  };
+
+  render(e) {
+    return (
+      <div className="todo-wrapper">
+        <div className="ToDo-Container">
+          <div className="ToDo-Content">
+            {this.state.list.map((item, key) => {
+              return (
+                <ToDoItem
+                  key={key}
+                  item={item.todo}
+                  deleteItem={this.deleteItem.bind(this, key)}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            value={this.state.todo}
+            placeholder="Your task.."
+            onChange={this.handleInput}
+            onKeyPress={this.handleKeyPress}
+          />
+          <button className="add-button" onClick={this.createNewToDoItem}>
+            +
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ToDo;
