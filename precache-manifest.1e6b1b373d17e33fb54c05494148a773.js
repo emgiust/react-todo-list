2fedb13493fb902d337e9af7eedcb4835ed439c9
Emily Giust <emilygiust@Emilys-MacBook-Pro.local>
2019-02-04 21:17:37 -0600

    Updates

---
+++ precache-manifest.1e6b1b373d17e33fb54c05494148a773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "c4e0f9fda3b40a1fe9cb",
+    "url": "/react-todo-list/static/js/main.c4e0f9fd.chunk.js"
+  },
+  {
+    "revision": "872633813eb3847f201b",
+    "url": "/react-todo-list/static/js/1.87263381.chunk.js"
+  },
+  {
+    "revision": "c4e0f9fda3b40a1fe9cb",
+    "url": "/react-todo-list/static/css/main.73deaa5a.chunk.css"
+  },
+  {
+    "revision": "5beb536c75a3113fc9cf71aff9df8e0b",
+    "url": "/react-todo-list/index.html"
+  }
+];
